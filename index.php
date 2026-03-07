52df6dc5e3478900698b3cc3ef5f43dba559b2da
GuiGato99 <guigtsantos123@gmail.com>
2026-03-06 22:44:52 -0300

    trabalho

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Dashboard</title>
+<link rel="stylesheet" href="dash.css">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="dashboard-container">
+
+
+<nav>
+<h2>Meu Sistema</h2>
+
+<ul>
+<li><a href="#"> 💾 Dashboard</a></li>
+<li><a href="#"> 👥 Usuários</a></li>
+<li><a href="#"> 📜 Relatórios</a></li>
+<li><a href="#"> ⚙️ Configurações</a></li>
+</ul>
+
+<div class="perfil">
+<p>✉️ Logado como:</p>
+<strong>👥 Guilherme Gato </strong>
+<br>
+<strong> 🔐 Ra: 101118<strong>
+</div>
+
+</nav>
+
+
+<main>
+<section class="container-cards">
+
+<article>
+<h2> 👥 Usuários</h2>
+<p>Gerenciar usuários cadastrados no sistema.</p>
+<a href="#" class="btn">Acessar</a>
+</article>
+
+<article>
+<h2>📜Relatórios</h2>
+<p>Visualizar relatórios e estatísticas.</p>
+<a href="#" class="btn">Acessar</a>
+</article>
+
+<article>
+<h2>⚙️Configurações</h2>
+<p>Alterar configurações do sistema.</p>
+<a href="#" class="btn">Acessar</a>
+</article>
+
+</section>
+</main>
+
+</div>
+
+<footer>
+<p> Trabalho Erick</p>
+<p> Sexta feria 06/03/2026<p>
+</footer>
+
+</body>
+</html>
